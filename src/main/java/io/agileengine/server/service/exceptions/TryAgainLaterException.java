0e7c60ada86346a7fcb9437e8c1499eda4761c47
Roman <roman.malko@gmail.com>
2020-08-10 12:49:38 +0300

    Delete TryAgainLaterException.java

--- src/main/java/io/agileengine/server/service/exceptions/TryAgainLaterException.java
+++
@@ -1,7 +0,0 @@
-package io.agileengine.server.service.exceptions;
-
-public class TryAgainLaterException extends RuntimeException {
-    public TryAgainLaterException() {
-        super("Try again later");
-    }
-}
